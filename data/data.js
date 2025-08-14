71af0486d3d6fcb5cd4fff59ec64bc389689a0cc
nroggero1 <86253224+nroggero1@users.noreply.github.com>
2025-08-13 23:09:20 -0300

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // Host de la base de datos
-  port: process.env.DB_PORT,       // Puerto (3306)
-  user: process.env.DB_USER,       // Usuario MySQL
-  password: process.env.DB_PASSWORD, // Contraseña MySQL
-  database: process.env.DB_NAME,   // Nombre de la base
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
+
 
